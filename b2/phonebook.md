a874c8d5573d3b73ab1c1fc75cb0833b32193cfe
Pito Salas <pitosalas@gmail.com>
2020-01-17 15:44:04 -0500

    more

--- b2/phonebook.md
+++ b2/phonebook.md
@@ -5,3 +5,7 @@
 5. If user types "+l" (i.e. a null string) print a list of all the names in a nice phonebook format
 6. If the user enters something that starts with "+n" then have user enter a new name.
 7. If the user enters "+s" then Save the list of names onto disk
+8. If the user enters "+r" then read the list of names back in
+9. Delete the part of your program that filled the list with Faker stuff.They are now going to be in the file on disk. 
+10. Automatically read in the last file when you startup. 
+11. YAY! You got an A+
--- demoagain/pstore.rb
+++ demoagain/pstore.rb
@@ -1,4 +1,5 @@
 require 'yaml/store'
+require 'faker'
 
 class Creature
   def initialize
@@ -7,7 +8,9 @@ class Creature
   end
 end
 
-store = YAML::Store.new('my_database.yaml') # Create or Load my_database.yaml file
+phonebook = [{ name: Faker::Name.name, addr: Faker::Address.city},
+{ name: Faker::Name.name, addr: Faker::Address.city},{ name: Faker::Name.name, addr: Faker::Address.city},{ name: Faker::Name.name, addr: Faker::Address.city},{ name: Faker::Name.name, addr: Faker::Address.city},{ name: Faker::Name.name, addr: Faker::Address.city}]
+store = YAML::Store.new('./phonebook') # Create or Load my_database.yaml file
 
 # Get Store Data
 my_var = store.transaction{store[:my_var]}
@@ -15,12 +18,13 @@ a_creature = Creature.new
 
 # Save Data
 store.transaction do
-  store[:foo] = 'bar'
-  store[:greeting] = ['Hello','World']
-  store[:amount] = 1.56
-  store[:children] = {george: 12, jane: 13, tammy: 0}
-  store[:last_accessed] = Time.now
-  store[:creature] = a_creature
+  # store[:foo] = 'bar'
+  # store[:greeting] = ['Hello','World']
+  # store[:amount] = 1.56
+  # store[:children] = {george: 12, jane: 13, tammy: 0}
+  # store[:last_accessed] = Time.now
+  # store[:creature] = a_creature
+  store[:all] = phonebook
 
   # Save Changes
   store.commit
